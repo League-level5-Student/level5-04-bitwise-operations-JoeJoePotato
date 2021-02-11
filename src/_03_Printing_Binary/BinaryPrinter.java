7ad7de9da21288a0eba395130e2761bdd5dd18e7
JoeJoePotato <34780073+JoeJoePotato@users.noreply.github.com>
2021-02-10 18:28:50 -0800

    FINISHED MOD 4

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -7,28 +7,64 @@ public class BinaryPrinter {
 	 * Use the main method to test your methods.
 	 */
 
+
 	
 	public void printByteBinary(byte b) {
 		// We first want to print the bit in the one's place
 		
 		// Shift b seven bits to the right
-		
+
 		// Use the & operator to "mask" the bit in the one's place
 		// This can be done by "anding" (&) it with the value of 1
-		
+
 		// Print the result using System.out.print (NOT System.out.println)
-		
+
 		//Use this method to print the remaining 7 bits of b
+		
+
+		
+		
+		System.out.print((b & 0b10000000) >> 7);
+		System.out.print((b & 0b01000000) >> 6);
+		System.out.print((b & 0b00100000) >> 5);
+		System.out.print((b & 0b00010000) >> 4);
+		System.out.print((b & 0b00001000) >> 3);
+		System.out.print((b & 0b00000100) >> 2);
+		System.out.print((b & 0b00000010) >> 1);
+		System.out.print((b & 0b00000001) >> 0);
+		
+		
 	}
 	
 	public void printShortBinary(short s) {
 		// Create 2 byte variables
-		
+		byte p=00000000;
+		byte k=00000000;
 		// Use bit shifting and masking (&) to save the first
 		// 8 bits of s in one byte, and the second 8 bits of
 		// s in the other byte
+	//00000000
+		k+= s& 0b0000000010000000 >> 7;
+		k+= s& 0b0000000001000000 >> 6;
+		k+= s& 0b0000000000100000 >> 5;
+		k+= s& 0b0000000000010000 >> 4;
+		k+= s& 0b0000000000001000 >> 3;
+		k+= s& 0b0000000000000100 >> 2;
+		k+= s& 0b0000000000000010 >> 1;
+		k+= s& 0b0000000000000001 >> 0;
 		
+	p+= s& 0b1000000000000000 >> 15;
+	p+= s& 0b0100000000000000 >> 14;
+	p+= s& 0b0010000000000000 >> 13;
+	p+= s& 0b0001000000000000 >> 12;
+	p+= s& 0b0000100000000000 >> 11;
+	p+= s& 0b0000010000000000 >> 10;
+	p+= s& 0b0000001000000000 >> 9;
+	p+= s& 0b0000000100000000 >> 8;
+
 		// Call printByteBinary twice using the two bytes
+	printByteBinary(p);
+	printByteBinary(k);
 		// Make sure they are in the correct order
 	}
 	
@@ -49,5 +85,8 @@ public class BinaryPrinter {
 	
 	public static void main(String[] args) {
 		// Test your methods here
+BinaryPrinter bip=new BinaryPrinter();
+//bip.printByteBinary((byte) 5);
+bip.printShortBinary((short) 129);
 	}
 }
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -37,19 +37,51 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if(base64Chars[i]==c) {
+				return (byte) i;
+			}
+		}
+		return (Byte) null;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		return null;
+		byte[] bs=new byte[4];
+		byte[] bm=new byte[3];
+		
+		for (int i = 0; i < bs.length; i++) {
+			bs[i]=convertBase64Char(s.charAt(i));
+		}
+		//shift donor byte to get first two bits. Mask then and with storage byte
+	bm[0]=(byte) ((bs[0]<<2)+(bs[1]>>4));
+	bm[1]=(byte) ((bs[1]<<4)+(bs[2]>>2));
+	bm[2]=(byte) ((bs[2]<<6)+(bs[3]));
+	
+		
+		return bm;
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
 	//   and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
-		return null;
+		String s="";
+		byte [] m=new byte[3]; 
+		byte [] b=new byte[file.length()*3/4]; 
+	int j=0;
+		for (int i = 0; i < file.length()-3; i+=4) {
+			System.out.println(i);
+			s=""+file.charAt(i)+file.charAt(i+1)+file.charAt(i+2)+file.charAt(i+3);
+			System.out.println(s);
+			m=convert4CharsTo24Bits(s);
+			b[j]=m[0];
+			b[j+1]=m[1];
+			b[j+2]=m[2];
+			j+=3;
+		}
+		return b;
+		
 	}
 }
